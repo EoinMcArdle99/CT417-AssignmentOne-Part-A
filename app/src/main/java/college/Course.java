b72b20e6f6d7fac4dde679a8584c39a099ec0477
EoinMcArdle99 <eoinmac995@gmail.com>
2021-09-29 10:01:32 +0100

    Init

---
+++ app/src/main/java/college/Course.java
@@ -0,0 +1,13 @@
+package college;
+import java.util.ArrayList;
+
+import org.joda.time.DateTime;
+
+public class Course {
+    private String name;
+    private ArrayList<Student> students;
+    private ArrayList<Module> modules;
+    private DateTime startDate;
+    private DateTime endDate;
+
+}
---
+++ app/src/main/java/college/Module.java
@@ -0,0 +1,10 @@
+package college;
+
+import java.util.ArrayList;
+
+public class Module {
+    private String name;
+    private String id;
+    private ArrayList<Student> students;
+    private ArrayList<Course> courses;
+}
