bf6be82a43d5779215b19955c0f0773ba4e75bc4
EoinMcArdle99 <eoinmac995@gmail.com>
2021-10-27 16:04:56 +0100

    Classes and tests

--- lib/src/main/java/PartA/Student.java
+++ lib/src/main/java/PartA/Student.java
@@ -6,77 +6,77 @@ public class Student {
     private int age;
     private String dob;
     private long id;
-    private String username;
-    // private ArrayList<Module> modules;
-    // private ArrayList<Course> course;
+    private ArrayList<Module> modules;
+    private ArrayList<Course> courses;
 
-    public Student() {
-        
+    public Student(String name, int age, String dob, long id) {
+     this.name = name;
+     this.age = age;
+     this.dob = dob;
+     this.id = id;
     }
 
-    // public Student(String name, int age, String dob, long id, ArrayList<Module> modules, ArrayList<Course> course) {
-    //     this.name = name;
-    //     this.age = age;
-    //     this.dob = dob;
-    //     this.id = id;
-    //     this.modules = modules;
-    //     this.course = course;
-    //     this.username = getUsername();
-    // }
+    public String getName() {
+     return this.name;
+    }
 
-    // public String getName() {
-    //     return this.name;
-    // }
+    public void setName(String name) {
+     this.name = name;
+    }
 
-    // public void setName(String name) {
-    //     this.name = name;
-    // }
+    public int getAge() {
+     return this.age;
+    }
 
-    // public int getAge() {
-    //     return this.age;
-    // }
+    public void setAge(int age) {
+     this.age = age;
+    }
 
-    // public void setAge(int age) {
-    //     this.age = age;
-    // }
+    public String getDob() {
+     return this.dob;
+    }
 
-    // public String getDob() {
-    //     return this.dob;
-    // }
+    public void setDob(String dob) {
+     this.dob = dob;
+    }
 
-    // public void setDob(String dob) {
-    //     this.dob = dob;
-    // }
+    public long getId() {
+     return this.id;
+    }
 
-    // public long getId() {
-    //     return this.id;
-    // }
+    public void setId(long id) {
+     this.id = id;
+    }
 
-    // public void setId(long id) {
-    //     this.id = id;
-    // }
+    public String getUsername() {
+     return this.name  + "-" + this.dob;
+    }
 
-    // public String getUsername() {
-    //     return this.name + this.dob;
-    // }
+    public ArrayList<Module> getModules() {
+     return this.modules;
+    }
 
-    // public void setUsername(String username) {
-    //     this.username = username;
-    // }
+    public void setModules(ArrayList<Module> modules) {
+     this.modules = modules;
+    }
 
-    // public ArrayList<Module> getModules() {
-    //     return this.modules;
-    // }
+    public ArrayList<Course> getCourses() { return this.courses; }
 
-    // public void setModules(ArrayList<Module> modules) {
-    //     this.modules = modules;
-    // }
+    public void setCourses(ArrayList<Course> courses) {
+     this.courses = courses;
+    }
 
-    // public ArrayList<Course> getCourse() {
-    //     return this.course;
-    // }
-
-    // public void setCourse(ArrayList<Course> course) {
-    //     this.course = course;
-    // }
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder(getUsername() + "\n");
+        str.append("Modules:\n");
+        for (Module module : modules) {
+            str.append("\t" + module.getName() + "\n");
+        }
+        str.append("Courses:\n");
+        for (Course course : courses) {
+            str.append("\t" + course.getName() + "\n");
+        }
+        return str.toString();
+    }
 }
---
+++ lib/src/test/java/PartA/StudentTest.java
@@ -0,0 +1,35 @@
+package PartA;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.joda.time.DateTime;
+
+public class StudentTest {
+    Student student;
+
+    @Before public void init() {
+        // Reset student test object before each test.
+        student = new Student("Eoin McArdle", 21, "05/12/1999", 198181929);
+    }
+
+    @Test public void getUserNameReturnsCorrectUsername() {
+        assertEquals("Eoin McArdle-05/12/1999", student.getUsername());
+    }
+
+    @Test public void modulesCanBeAddedToStudent() {
+        Module module = new Module("Software Engineering","CT417");
+        ArrayList<Module> modules = new ArrayList<Module>();
+        modules.add(module);
+        student.setModules(modules);
+        assertEquals(modules, student.getModules());
+    }
+
+    @Test public void coursesCanBeAddedToStudent() {
+        Course course = new Course("CS & IT",DateTime.now(), DateTime.now());
+        ArrayList<Course> courses = new ArrayList<Course>();
+        courses.add(course);
+        student.setCourses(courses);
+        assertEquals(courses, student.getCourses());
+    }
+}
