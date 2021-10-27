3161b4ae544514445d519a01f8c1360f616b8944
EoinMcArdle99 <eoinmac995@gmail.com>
2021-10-27 20:35:39 +0100

    Added war plugin

--- lib/src/test/java/PartA/StudentTest.java
+++ lib/src/test/java/PartA/StudentTest.java
@@ -32,4 +32,17 @@ public class StudentTest {
         student.setCourses(courses);
         assertEquals(courses, student.getCourses());
     }
+
+    @Test public void toStringOutputsCorrectRepresentation() {
+        Module module = new Module("Software Engineering","CT417");
+        ArrayList<Module> modules = new ArrayList<Module>();
+        modules.add(module);
+        student.setModules(modules);
+        Course course = new Course("CS & IT",DateTime.now(), DateTime.now());
+        ArrayList<Course> courses = new ArrayList<Course>();
+        courses.add(course);
+        student.setCourses(courses);
+        String out = student.toString();
+        assertEquals("Eoin McArdle-05/12/1999\nModules:\n\tSoftware Engineering\nCourses:\n\tCS & IT\n", out);
+    }
 }
